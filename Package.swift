dfffba9eeaadacd448e88496c8c7773fbd83f88b
TBXark <tbxark@outlook.com>
2021-12-28 14:27:35 +0800

    删除xcode项目文件

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DefaultAppUtils",
+    products: [
+        .executable(name: "DefaultAppUtils", targets: ["DefaultAppUtils"])
+    ],
+    dependencies: [
+	],
+    targets: [
+        .target(
+            name: "DefaultAppUtils",
+            dependencies: [
+            ])
+    ]
+)
